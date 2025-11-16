1c68f8c70cf0106803cc2b291c3dc874aa9cd765
riskydb <dbrisky1@gmail.com>
2025-11-16 23:52:03 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -337,7 +337,7 @@ function provisioning_download() {
     if [[ -n $auth_token ]];then
         wget --header="Authorization: Bearer $auth_token" -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1" -O "$3"
     else
-        wget -qnc --content-disposition --show-progress -e dotbytes="${3:-4M}" -P "$2" "$1" -O "$3"
+        wget -qnc --content-disposition --show-progress -e dotbytes="${4:-4M}" -P "$2" "$1" -O "$3"
     fi
 }
 
